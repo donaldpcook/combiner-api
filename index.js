6f6dae859b4b3e0cbf6bada3fecf0b6f9da4bd50
Donald Cook <Donald.Cook@rga.com>
2014-11-15 11:25:41 -0600

    Added POSTing ability

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express'),
+    util = require('util'),
+    fs = require('fs'),
+    busboy = require('connect-busboy'),
+    path = require('path'),
     gm = require('gm'),
     imageMagick = gm.subClass({ imageMagick: true }),
-    bodyParser = require('body-parser'),
     AWS = require('aws-sdk');
 
 var app = express();
@@ -9,6 +12,8 @@ var app = express();
 var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
 var S3_BUCKET = process.env.S3_BUCKET;
+
+app.use(busboy());
 
 //cors
 app.use(function(req, res, next) {
@@ -29,7 +34,7 @@ app.get('/', function(req, res) {
     return gm;
   };
 
-  var image = imageMagick();
+  var image = gm();
 
   combineImages(image)
     .toBuffer('jpg', function(err, buffer) {
@@ -52,7 +57,73 @@ app.get('/', function(req, res) {
           }
         });
       }
+      console.log('noooo');
     });
+});
+
+app.post('/', function(req, res) {
+
+  var files = [];
+  var imageNames = [];
+
+  req.busboy.on('file', function (fieldname, file, filename) {
+    file.on('data', function(chunk) {
+      files.push(chunk);
+    });
+  });
+
+  req.busboy.on('finish', function() {
+    var image = gm();
+    combineImages(image)
+      .toBuffer('jpg', function(err, buffer) {
+        if (!err) {
+          AWS.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+          var s3 = new AWS.S3();
+          var imageName = Math.floor(Math.random() * 999999) + '.jpg';
+
+          s3.putObject({
+            Bucket: S3_BUCKET,
+            Key: imageName,
+            Body: buffer,
+            ACL: 'public-read',
+            ContentType: 'image/jpeg'
+          }, function(err) {
+            if (!err) {
+              res.send('https://s3.amazonaws.com/' + S3_BUCKET + '/' + imageName);
+            } else {
+              console.log(err);
+            }
+          });
+
+          // cleanup tmp files
+          imageNames.forEach(function(imageName) {
+            fs.unlink(imageName, function() {
+            });
+          });
+        } else {
+          console.log(err);
+        }
+      });
+  });
+
+  req.pipe(req.busboy);
+
+  var combineImages = function(gm) {
+    if (!files) { return gm; }
+
+    files.forEach(function(image) {
+      var fileName = './tmp/' + Math.floor(Math.random() * 999999) + '.jpg';
+      var file = fs.createWriteStream(fileName);
+      file.write(image);
+      file.end();
+
+      imageNames.push(fileName);
+
+      gm.geometry(100, 100).append(fileName);
+    }, this);
+
+    return gm;
+  };
 });
 
 app.listen(process.env.PORT || 5000);
